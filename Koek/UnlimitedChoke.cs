8815efd31b5e07d17b60e8ac72b048a44d06449a
Sander Saares <sasaares@microsoft.com>
2020-12-27 08:18:00 +0200

    Add UnlimitedChoke.Instance

--- Koek/UnlimitedChoke.cs
+++ Koek/UnlimitedChoke.cs
@@ -5,6 +5,8 @@
     /// </summary>
     public sealed class UnlimitedChoke : IChoke
     {
+        public static UnlimitedChoke Instance { get; } = new UnlimitedChoke();
+
         public DataRate RateLimit { get; } = DataRate.FromBitsPerSecond(long.MaxValue);
         public long BucketSizeBytes => long.MaxValue;
         public bool RequestBytes(ushort count) => true;
