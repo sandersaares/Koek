8f6ea47558fd147ddc47e2fbf62b90c543c18daf
Sander Saares <sasaares@microsoft.com>
2021-02-03 08:21:21 +0200

    Thread.HasBeenStarted()

---
+++ Koek/ExtensionsForThread.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+
+namespace Koek
+{
+    public static class ExtensionsForThread
+    {
+        /// <summary>
+        /// Whether the thread has ever been started.
+        /// </summary>
+        public static bool HasBeenStarted(this Thread thread)
+        {
+            return (thread.ThreadState & ThreadState.Unstarted) != ThreadState.Unstarted;
+        }
+    }
+}
